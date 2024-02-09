4085ebc11bb73fd1bfa014522c775dc7ca3616ea
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-02-09 20:22:55 +0530

    adding styling

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ export default function App() {
       <Header /> {/* Render the Header component */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign_in" element={<Signin />} />
-        <Route path="/sign_out" element={<Signout />} />
-        <Route path="/profile" element={<Profile />} /> {/* Use the Profile component */}
+        <Route path="/About" element={<About />} />
+        <Route path="/Sign-In" element={<Signin />} />
+        <Route path="/Sign-Out" element={<Signout />} />
+        <Route path="/Profile" element={<Profile />} /> {/* Use the Profile component */}
       </Routes>
     </Router>
   );
