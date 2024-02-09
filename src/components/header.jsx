4fa08a52cb3e6af61c7ca341931929d736e46ba3
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-02-09 18:20:56 +0530

    adding tailwing css part

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,10 +3,8 @@ import React from 'react'
 export default function header() {
   return (
     <header>
-
-
-      
-      Header
+<span className='text-slate-500'>Tron</span>
+<span className='text-slate-500'>Estate</span>
     </header>
   )
 }
