2c5f39f340711a859520fd7e02ffdbd13800e06d
Kanish Mahato <72163903+tron2003@users.noreply.github.com>
2024-02-09 18:18:43 +0530

    adding header part for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import React, { Profiler } from 'react'
-import {BroswerRouter,Routes,Route} from "react-router-dom"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Signin from "./pages/signin";
+import Signout from "./pages/signout";
+import About from "./pages/about";
+import Header from "./components/header";
+import Profile from "./pages/profile"; // Import the Profile component
+
 export default function App() {
   return (
-  <BroswerRouter>
-  <Routes>
-< Route path="/" element={<home />}/>
-< Route path="/about" element={<about />}/>
-< Route path="/sign_in" element={<signin />}/>
-< Route path="/sign_out" element={<signout />}/>
-< Route path="/profile" element={<profile />}/>
-
-  </Routes>
-  </BroswerRouter>
-
-  )
+    <Router>
+      <Header /> {/* Render the Header component */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign_in" element={<Signin />} />
+        <Route path="/sign_out" element={<Signout />} />
+        <Route path="/profile" element={<Profile />} /> {/* Use the Profile component */}
+      </Routes>
+    </Router>
+  );
 }
